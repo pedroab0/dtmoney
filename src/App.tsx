06aee06059ea6cb67b64d280ee83317fd0670632
Pedro Barbosa <p.brbsa@gmail.com>
2022-05-18 00:03:45 -0300

    finising the construction of the components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
+import { useState } from "react";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
-import { GlobalStyle } from "./styles";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+import { TransactionsProvider } from "./hooks/useTransactions";
+import { GlobalStyle } from "./styles/styles";
 
 export function App() {
+	const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+	const handleOpenNewTransactionModal = () => setIsNewTransactionModalOpen(true);
+	const handleCloseNewTransactionModal = () => setIsNewTransactionModalOpen(false);
+
 	return (
-		<>
+		<TransactionsProvider>
 			<GlobalStyle />
-			<Header />
+			<Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
 			<Dashboard />
-		</>
+
+			<NewTransactionModal
+				isOpen={isNewTransactionModalOpen}
+				onRequestClose={handleCloseNewTransactionModal}
+			/>
+		</TransactionsProvider>
 	);
 }
 
